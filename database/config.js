4ca0242ad83c393cadf9bbd5c3043a56f969e6b7
Christopher Wong <chriskwong91@gmail.com>
2016-10-14 11:03:30 -0700

    Mongo Db

--- database/config.js
+++ database/config.js
@@ -5,7 +5,7 @@ var mongoose = require('mongoose');
 // var db = require('../config/env/config.js').mongo;
 // mongoose.connect(db.url); //connect to database
 var db = mongoose.connection;
-mongoose.connect('mongodb://localhost/codedrop/testingService');
+mongoose.connect('mongodb://heroku_lg728wpm:fsm72ufsoqp0covp5grj305dsj@ds021434.mlab.com:21434/heroku_lg728wpm');
 
 db.on('error', console.error);
 db.once('open', () => {
