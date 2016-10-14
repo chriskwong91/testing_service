3f1e5d6b9a16de2d30dd5a7dda5353c7b4e418c0
Christopher Wong <chriskwong91@gmail.com>
2016-10-14 14:30:00 -0700

    changed to deploy server mongodb

--- database/config.js
+++ database/config.js
@@ -5,8 +5,8 @@ var mongoose = require('mongoose');
 // var db = require('../config/env/config.js').mongo;
 // mongoose.connect(db.url); //connect to database
 var db = mongoose.connection;
-// mongoose.connect('mongodb://heroku_lg728wpm:fsm72ufsoqp0covp5grj305dsj@ds021434.mlab.com:21434/heroku_lg728wpm');
-mongoose.connect('mongodb://localhost/codedrop');
+mongoose.connect('mongodb://heroku_lg728wpm:fsm72ufsoqp0covp5grj305dsj@ds021434.mlab.com:21434/heroku_lg728wpm');
+// mongoose.connect('mongodb://localhost/codedrop');
 
 db.on('error', console.error);
 db.once('open', () => {
